9723986ebf8001a6e67d92636777808090a7392c
José <josemanuellopesf19@gmail.com>
2021-02-06 18:56:35 +0000

    js v final

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var current_page = 1;
+
 function procura() {
   //codigo de acesso a API
   var access_token = "dd4e1cb73ca3a1036d4e98d26f72a439141dc17039e1ae79b7bc2a23f3488578";
   //pagina atual esta sempre com o valor 1, mas depois deverá ser calculada
-  var current_page = 1; //get_current_page_number();
+  //var current_page = 1; //get_current_page_number();
   //endereço para ir buscar as imagens mais recentes
   var endereco = ("https://api.unsplash.com/photos?per_page=24&page=" + current_page + "&order_by=latest&client_id=" + access_token);
 
@@ -17,118 +19,90 @@ function procura() {
 }
 
 
-function adicionarImagem(dataResposta) {
-  $('#contentorImagem').empty();
-  var linkDaPrimeiraImagem = dataResposta[0]["urls"]["regular"];
-  //ALERTA SÓ PARA VER QUE JÀ ESTÁ A CONSEGUIR OBTER IMAGENS
-  alert("TENHO IMAGENS!! Por exemplo a primeira tem este link: " + linkDaPrimeiraImagem);
+  function adicionarImagem(data) {
+    $('#contentorImagem').empty();
+    //var linkDaPrimeiraImagem = dataResposta[0]["urls"]["regular"];
+    //ALERTA SÓ PARA VER QUE JÀ ESTÁ A CONSEGUIR OBTER IMAGENS
+    //alert("TENHO IMAGENS!! Por exemplo a primeira tem este link: " + linkDaPrimeiraImagem);
 
-  var arrayDeImagem = dataResposta;
+    var arrayDeImagem = data;
 
-  for (var i = 0; i < arrayDeImagem.length; i++) {
-    var imagem = arrayDeImagem[i];
-    criarImagem(imagem);
+    for (var i = 0; i < arrayDeImagem.length; i++) {
+      var imagem = arrayDeImagem[i];
+      criarImagem(imagem);
+    }
   }
-}
-
-/*var arrayDeFotos = data;
-  for (var i=0; i<arrayDeFotos.length; i++) {
-    var foto = arrayDeFotos[i];
-    criarFoto(foto);
-  }*/
-
-function criarImagem(imagem) {
-  var h4 = document.createElement("h4");
-  h4.className = "card-title";
-  h4.innerText = imagem.title;
-
-  /* // criar h5
-  var h5 = document.createElement("h5");
-  h5.className = "card-title";
-  h5.innerText = foto.user.name;*/
-
-  var div = document.createElement("div");
-  div.className = "card-body";
-  div.appendChild(h4);
 
 
-/* // criar div filha
-  var div = document.createElement("div");
-  div.className = "card-body";
-  div.appendChild(h5);*/
+  function criarImagem(imagem) {
+    // criar h4
+    var h4 = document.createElement("h4");
+    h4.className = "card-title";
+    h4.innerText = imagem.user.name;
 
-  var img = document.createElement("img");
-  img.className = "card-img-top";
-  var imgSrc = imagem["urls"]["raw"] + "&fit=crop&w=500&h=500";
-  img.setAttribute("src", imgSrc);
-
- /*// criar img
- var img = document.createElement("img");
- img.className = "card-img-top";
- var imgSrc = foto.urls.full;
- img.setAttribute("src", imgSrc);*/
-
-  var divPrincipal = document.createElement("div");
-  divPrincipal.className = "card col-3";
-  divPrincipal.appendChild(img);
-  divPrincipal.appendChild(div);
-
- /*// criar div pai
- var divPrincipal = document.createElement("div");
- divPrincipal.className = "card col-3";
- divPrincipal.appendChild(img);
- divPrincipal.appendChild(div);*/
+    var description = document.createElement("description");
+    description.className = "card-text";
+    description.innerText = imagem.alt_description;
 
 
-  var container = document.getElementById("contentorImagem");
-  container.appendChild(divPrincipal);
-}
 
-/*
-  // adicionar div pai à pagina/DOM
-  var container = document.getElementById("contentorFotos");
-  container.appendChild(divPrincipal);
-}*/
+    // criar div filha
+    var div = document.createElement("div");
+    div.className = "card-body";
+    div.appendChild(h4);
 
-function programarCarregamentoPagina() {
-  $(window).on("load", procura);
-}
+    // criar img
+    var img = document.createElement("img");
+    img.className = "card-img-top";
+    var imgSrc = imagem["urls"]["raw"] + "&fit=crop&w=500&h=500";
+    img.setAttribute("src", imgSrc);
 
-function anterior() {
-  if (pagina == 1)
-    alert("NAO PERMITIDO");
-  else {
-    pagina = pagina - 1;
+
+    // criar div pai
+    var divPrincipal = document.createElement("div");
+    divPrincipal.className = "card col-12 col-lg-3 col-md-4 col-sm-6 cardEdit";
+    divPrincipal.appendChild(img);
+    divPrincipal.appendChild(div);
+    divPrincipal.appendChild(description);
+
+    //criar icon 
+    var icon = document.createElement("icon");
+    icon.className = "card-title";
+    icon.innerText = imagem["urls"]["regular"];
+
+    // adicionar div pai à pagina/DOM
+    var container = document.getElementById("contentorImagem");
+    container.appendChild(divPrincipal);
+  }
+
+
+  function programarCarregamentoPagina() {
+    $(window).on("load", procura);
+  }
+
+  function anterior() {
+    if (current_page == 1)
+      alert("NÃO ACEITE");
+    else {
+      current_page--;
+      procura();
+    }
+  }
+
+  function seguinte() {
+    current_page++;
     procura();
   }
-}
 
-function seguinte() {
-  pagina = pagina + 1;
-  procura();
-}
+  function programarBotoesPaginacao() {
+    var botaoAnterior = document.getElementById("anterior");
+    var botaoSeguinte = document.getElementById("seguinte");
 
-function programarBotoesPaginacao() {
-  var botaoAnterior = document.getElementById("anterior");
-  var botaoSeguinte = document.getElementById("seguinte");
+    botaoAnterior.addEventListener("click", anterior);
+    botaoSeguinte.addEventListener("click", seguinte);
+  }
 
-  botaoAnterior.addEventListener("click", anterior);
-  botaoSeguinte.addEventListener("click", seguinte);
-}
+  programarCarregamentoPagina();
+  programarBotoesPaginacao();
 
-programarCarregamentoPagina();
-programarBotoesPaginacao();
-
-var pagina = 1;
-
-
-
-
-
- 
-
- 
-
- 
-
- 
+  var pagina = 1;
