8c65701e428287141f77f6382e81aec8a2efdf2c
Lucas Silva <lucasmsilva1503@gmail.com>
2021-02-06 19:30:02 +0000

    Versao Final

--- index.js
+++ index.js
@@ -19,90 +19,90 @@ function procura() {
 }
 
 
-  function adicionarImagem(data) {
-    $('#contentorImagem').empty();
-    //var linkDaPrimeiraImagem = dataResposta[0]["urls"]["regular"];
-    //ALERTA SÓ PARA VER QUE JÀ ESTÁ A CONSEGUIR OBTER IMAGENS
-    //alert("TENHO IMAGENS!! Por exemplo a primeira tem este link: " + linkDaPrimeiraImagem);
+function adicionarImagem(data) {
+  $('#contentorImagem').empty();
+  //var linkDaPrimeiraImagem = dataResposta[0]["urls"]["regular"];
+  //ALERTA SÓ PARA VER QUE JÀ ESTÁ A CONSEGUIR OBTER IMAGENS
+  //alert("TENHO IMAGENS!! Por exemplo a primeira tem este link: " + linkDaPrimeiraImagem);
 
-    var arrayDeImagem = data;
+  var arrayDeImagem = data;
 
-    for (var i = 0; i < arrayDeImagem.length; i++) {
-      var imagem = arrayDeImagem[i];
-      criarImagem(imagem);
-    }
+  for (var i = 0; i < arrayDeImagem.length; i++) {
+    var imagem = arrayDeImagem[i];
+    criarImagem(imagem);
   }
+}
 
 
-  function criarImagem(imagem) {
-    // criar h4
-    var h4 = document.createElement("h4");
-    h4.className = "card-title";
-    h4.innerText = imagem.user.name;
+function criarImagem(imagem) {
+  // criar h4
+  var h4 = document.createElement("h4");
+  h4.className = "card-title";
+  h4.innerText = imagem.user.name;
 
-    var description = document.createElement("description");
-    description.className = "card-text";
-    description.innerText = imagem.alt_description;
-
-
-
-    // criar div filha
-    var div = document.createElement("div");
-    div.className = "card-body";
-    div.appendChild(h4);
-
-    // criar img
-    var img = document.createElement("img");
-    img.className = "card-img-top";
-    var imgSrc = imagem["urls"]["raw"] + "&fit=crop&w=500&h=500";
-    img.setAttribute("src", imgSrc);
+  var description = document.createElement("description");
+  description.className = "card-text";
+  description.innerText = imagem.alt_description;
 
 
-    // criar div pai
-    var divPrincipal = document.createElement("div");
-    divPrincipal.className = "card col-12 col-lg-3 col-md-4 col-sm-6 cardEdit";
-    divPrincipal.appendChild(img);
-    divPrincipal.appendChild(div);
-    divPrincipal.appendChild(description);
 
-    //criar icon 
-    var icon = document.createElement("icon");
-    icon.className = "card-title";
-    icon.innerText = imagem["urls"]["regular"];
+  // criar div filha
+  var div = document.createElement("div");
+  div.className = "card-body";
+  div.appendChild(h4);
 
-    // adicionar div pai à pagina/DOM
-    var container = document.getElementById("contentorImagem");
-    container.appendChild(divPrincipal);
-  }
+  // criar img
+  var img = document.createElement("img");
+  img.className = "card-img-top";
+  var imgSrc = imagem["urls"]["raw"] + "&fit=crop&w=500&h=500";
+  img.setAttribute("src", imgSrc);
 
 
-  function programarCarregamentoPagina() {
-    $(window).on("load", procura);
-  }
+  // criar div pai
+  var divPrincipal = document.createElement("div");
+  divPrincipal.className = "card col-12 col-lg-3 col-md-4 col-sm-6 cardEdit";
+  divPrincipal.appendChild(img);
+  divPrincipal.appendChild(div);
+  divPrincipal.appendChild(description);
 
-  function anterior() {
-    if (current_page == 1)
-      alert("NÃO ACEITE");
-    else {
-      current_page--;
-      procura();
-    }
-  }
+  //criar icon 
+  var icon = document.createElement("icon");
+  icon.className = "card-title";
+  icon.innerText = imagem["urls"]["regular"];
 
-  function seguinte() {
-    current_page++;
+  // adicionar div pai à pagina/DOM
+  var container = document.getElementById("contentorImagem");
+  container.appendChild(divPrincipal);
+}
+
+
+function programarCarregamentoPagina() {
+  $(window).on("load", procura);
+}
+
+function anterior() {
+  if (current_page == 1)
+    alert("NÃO ACEITE");
+  else {
+    current_page--;
     procura();
   }
+}
 
-  function programarBotoesPaginacao() {
-    var botaoAnterior = document.getElementById("anterior");
-    var botaoSeguinte = document.getElementById("seguinte");
+function seguinte() {
+  current_page++;
+  procura();
+}
 
-    botaoAnterior.addEventListener("click", anterior);
-    botaoSeguinte.addEventListener("click", seguinte);
-  }
+function programarBotoesPaginacao() {
+  var botaoAnterior = document.getElementById("anterior");
+  var botaoSeguinte = document.getElementById("seguinte");
 
-  programarCarregamentoPagina();
-  programarBotoesPaginacao();
+  botaoAnterior.addEventListener("click", anterior);
+  botaoSeguinte.addEventListener("click", seguinte);
+}
 
-  var pagina = 1;
+programarCarregamentoPagina();
+programarBotoesPaginacao();
+
+var pagina = 1;
